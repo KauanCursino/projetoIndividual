4e0b698e33f3dab83c1276ee851afeb13a44a72b
KauanCursino <kauan.souza@sptech.school>
2023-06-05 10:18:14 -0300

    aJUSTE NO bd

--- web-data-viz-main/web-data-viz-main/site/src/database/scriptVolei.sql
+++ web-data-viz-main/web-data-viz-main/site/src/database/scriptVolei.sql
@@ -15,7 +15,11 @@ senha VARCHAR(45)
 
 CREATE TABLE selecao(
 idSelecao INT PRIMARY KEY AUTO_INCREMENT,
-paisSede VARCHAR(45)
+paisSede VARCHAR(45),
+apelidoSelecao VARCHAR(50),
+descricaoSelecao VARCHAR(500),
+qtdTitulosMundiais INT,
+qtdTitulosOlimpiadas INT
 );
 CREATE TABLE jogador(
 idJogador INT PRIMARY KEY AUTO_INCREMENT,
