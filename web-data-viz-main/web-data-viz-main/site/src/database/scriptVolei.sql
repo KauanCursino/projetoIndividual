1ffddef74617067021090df3f20b0d16e875e93a
KauanCursino <kauan.souza@sptech.school>
2023-06-11 20:13:01 -0300

    inicio Dashboard e Sobre Mim

--- web-data-viz-main/web-data-viz-main/site/src/database/scriptVolei.sql
+++ web-data-viz-main/web-data-viz-main/site/src/database/scriptVolei.sql
@@ -1,14 +1,17 @@
 CREATE DATABASE KCsVolei;
 
 USE KCsVolei;
-
+ 
 CREATE TABLE usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(45),
-altura INT
+altura INT,
+fkLogin INT,
+FOREIGN KEY (fkLogin) REFERENCES login(idLogin)
 );
 
 CREATE TABLE login(
+idLogin INT PRIMARY KEY AUTO_INCREMENT,
 email VARCHAR(45),
 senha VARCHAR(45)
 );
@@ -25,6 +28,11 @@ CREATE TABLE jogador(
 idJogador INT PRIMARY KEY AUTO_INCREMENT,
 nomeJogador VARCHAR(45),
 posicaoJogador VARCHAR(45),
+altura INT,
 fkSelecao INT,
 FOREIGN KEY (fkSelecao) REFERENCES selecao(idSelecao)
 );
+
+SELECT * FROM usuario;
+SELECT * FROM login;
+delete FROM usuario WHERE idUsuario = 1;
--- web-data-viz-main/web-data-viz-main/site/src/models/medidaModel.js
+++ web-data-viz-main/web-data-viz-main/site/src/models/medidaModel.js
@@ -6,21 +6,15 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
+        AVG(altura)
+                    from jogador
+                    where fkSelecao = ${idSelecao}
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
+        AVG(altura)
+                    from jogador
+                    where fkSelecao = ${idSelecao}
                     order by id desc limit ${limite_linhas}`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
@@ -37,20 +31,16 @@ function buscarMedidasEmTempoReal(idAquario) {
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
+        AVG(altura), 
+                        fkSelecao
+                        from jogador where fkSelecao= ${idSelecao} 
                     order by id desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
+        AVG(altura), 
+                        fkSelecao
+                        from medida where fkSelecao= ${idSelecao} 
                     order by id desc limit 1`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
